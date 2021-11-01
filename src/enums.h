f11241eb2fa80dd55e4460f474b40510d8abd2a0
Nicholas Phillips <nwp2@illinois.edu>
2021-11-01 16:46:03 -0500

    first draft interface

---
+++ src/enums.h
@@ -0,0 +1,18 @@
+//
+// Created by 16182 on 11/1/2021.
+//
+
+#ifndef RENDER_ENGINE_ENUMS_H
+#define RENDER_ENGINE_ENUMS_H
+
+enum class SuccessCode {
+    SUCCESS,
+    FAILURE
+};
+
+enum class EngineType {
+    OPENGL,
+    VULKAN
+};
+
+#endif //RENDER_ENGINE_ENUMS_H
