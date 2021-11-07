76c0d4214bcf26da6a66314ed5375134e1ce5b59
Jebbly <jeffliu604@gmail.com>
2021-11-07 01:46:46 -0600

    Add option for invalid engine type

--- src/RenderEngine.cpp
+++ src/RenderEngine.cpp
@@ -6,12 +6,16 @@
 std::unique_ptr<RenderEngine> RenderEngine::make_engine(EngineType type) {
   switch (type) {
   case EngineType::VULKAN: {
-      std::cout << "CREATING VULKAN ENGINE" << std::endl;
+    std::cout << "CREATING VULKAN ENGINE" << std::endl;
     return std::unique_ptr<RenderEngine>(new VkRenderEngine());
   }
   case EngineType::OPENGL: {
-      std::cout << "CREATING OPENGL ENGINE" << std::endl;
+    std::cout << "CREATING OPENGL ENGINE" << std::endl;
     return std::unique_ptr<RenderEngine>(new GlRenderEngine());
+  }
+  default: {
+    std::cerr << "Invalid engine type provided\n";
+    return nullptr;
   }
   }
 }
